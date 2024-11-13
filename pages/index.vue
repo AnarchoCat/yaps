e4e1ce2a054ed5a1af75baf6c215eb7cd9ec46d5
Anarcho Cat <d2717p@gmail.com>
2024-11-13 16:10:09 +0900

    Update index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <h1>首页</h1>
-    <div class="absolute w-full h-full top-0 left-0 flex items-center justify-center text-8xl animate-bounce">🐺🍆🦊</div>
+    <div class="mx-auto w-fit text-8xl animate-spin text-center">🙀</div>
+    <article class="text-sm md:text-base mx-auto w-fit p-4 border-2 border-rose-600 shadow shadow-red-800">
+      <p class="text-center">天生万物以养人，<br class="hidden" />世人犹怨天不仁。</p>
+      <p class="text-center">不知蝗蠹遍天下，<br class="hidden" />苦尽苍生尽王臣。</p>
+      <p class="text-center">人之生矣有贵贱，<br class="hidden" />贵人长为天恩眷。</p>
+      <p class="text-center">人生富贵总由天，<br class="hidden" />草民之穷由天谴。</p>
+      <p class="text-center">忽有狂徒夜磨刀，<br class="hidden" />帝星飘摇荧惑高。</p>
+      <p class="text-center">翻天覆地从今始，<br class="hidden" />杀人何须惜手劳。</p>
+      <p class="text-center">不忠之人杀！<br class="hidden" />不孝之人杀！</p>
+      <p class="text-center">不仁之人杀！<br class="hidden" />不义之人杀！</p>
+      <p class="text-center">不礼不智不信人，<br class="hidden" />大西王曰杀杀杀！</p>
+      <p class="text-center">我生不为逐鹿来，<br class="hidden" />都门懒筑黄金台，</p>
+      <p class="text-center">状元百官都如狗，<br class="hidden" />总是刀下觳觫材。</p>
+      <p class="text-center">传令麾下四王子，<br class="hidden" />破城不须封刀匕。</p>
+      <p class="text-center">山头代天树此碑，<br class="hidden" />逆天之人立死跪亦死！</p>
+    </article>
   </div>
 </template>
