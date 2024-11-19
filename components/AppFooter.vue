3d285c14dbc7f36874ec3083474469ade7f2eb2e
Anarcho Cat <d2717p@gmail.com>
2024-11-19 15:24:38 +0900

    Add github icon

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -6,10 +6,13 @@ import { EnvelopeIcon } from '@heroicons/vue/24/outline'
   <footer class="py-4 md:py-8 mt-4 border-t flex flex-col gap-4 items-center mx-auto md:w-fit md:px-16">
     <div class="flex gap-2 justify-center items-center">
       <NuxtLink to="mailto:d2717p@gmail.com">
-        <EnvelopeIcon class="w-6 h-6" />
+        <EnvelopeIcon class="size-6" />
       </NuxtLink>
       <NuxtLink to="//x.com/lyhng_rika">
-        <IconX class="w-5 h-5" />
+        <IconX class="size-5" />
+      </NuxtLink>
+      <NuxtLink to="//github.com/AnarchoCat/yaps">
+        <IconGithub class="size-6" />
       </NuxtLink>
     </div>
     <p class="text-center">&copy; 2024 Rica the Botsuraku-Reijō. All rights reserved.</p>
