7d8507ad363af1caf437737fb0f432fb1441be8a
Anarcho Cat <d2717p@gmail.com>
2024-11-19 10:04:05 +0900

    Remove chevron when no children

--- components/content/Toc.vue
+++ components/content/Toc.vue
@@ -3,25 +3,25 @@
     <li v-for="link in value" :key="link.id">
       <div class="flex justify-between items-center gap-4 peer">
         <NuxtLink :to="`#${link.id}`" @click="$emit('jump')">{{ link.text }}</NuxtLink>
-        <label><input type="checkbox" hidden class="peer" :checked="depth > 1" /><ChevronDownIcon class="w-4 h-4 peer-checked:rotate-180 transition-transform" /></label>
+        <label><input type="checkbox" hidden class="peer" :checked="depth > 1" /><ChevronDownIcon v-if="link.children" class="w-4 h-4 peer-checked:rotate-180 transition-transform" /></label>
       </div>
       <ul class="ml-4 max-h-0 peer-has-[:checked]:max-h-96 overflow-hidden transition-all">
         <li v-for="link1 in link.children" :key="link1.id">
           <div class="flex justify-between items-center gap-4 peer">
             <NuxtLink :to="`#${link1.id}`" @click="$emit('jump')">{{ link1.text }}</NuxtLink>
-            <label><input type="checkbox" hidden class="peer" :checked="depth > 2" /><ChevronDownIcon class="w-4 h-4 peer-checked:rotate-180 transition-transform" /></label>
+            <label><input type="checkbox" hidden class="peer" :checked="depth > 2" /><ChevronDownIcon v-if="link1.children" class="w-4 h-4 peer-checked:rotate-180 transition-transform" /></label>
           </div>
           <ul class="ml-8 max-h-0 peer-has-[:checked]:max-h-96 overflow-hidden transition-all">
             <li v-for="link2 in link1.children" :key="link2.id">
               <div class="flex justify-between items-center gap-4 peer">
                 <NuxtLink :to="`#${link2.id}`" @click="$emit('jump')">{{ link2.text }}</NuxtLink>
-                <label><input type="checkbox" hidden class="peer" :checked="depth > 3" /><ChevronDownIcon class="w-4 h-4 peer-checked:rotate-180 transition-transform" /></label>
+                <label><input type="checkbox" hidden class="peer" :checked="depth > 3" /><ChevronDownIcon v-if="link2.children" class="w-4 h-4 peer-checked:rotate-180 transition-transform" /></label>
               </div>
               <ul class="ml-8 max-h-0 peer-has-[:checked]:max-h-96 overflow-hidden transition-all">
                 <li v-for="link3 in link2.children" :key="link3.id">
                   <div class="flex justify-between items-center gap-4 peer">
                     <NuxtLink :to="`#${link3.id}`" @click="$emit('jump')">{{ link3.text }}</NuxtLink>
-                    <label><input type="checkbox" hidden class="peer" :checked="depth > 4" /><ChevronDownIcon class="w-4 h-4 peer-checked:rotate-180 transition-transform" /></label>
+                    <label><input type="checkbox" hidden class="peer" :checked="depth > 4" /><ChevronDownIcon v-if="link3.children" class="w-4 h-4 peer-checked:rotate-180 transition-transform" /></label>
                   </div>
                   <ul class="ml-8 max-h-0 peer-has-[:checked]:max-h-96 overflow-hidden transition-all">
                     <li v-for="link4 in link3.children" :key="link4.id">
